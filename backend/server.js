8b40913bb0ba3475c70a43a9c3934c8d611a8dfb
PC <saleh.majeed8@gmail.com>
2023-10-04 02:33:18 +0530

    added boiler code with help of juber and mofique

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+
+const corsOptions = {
+  origin: 'http://127.0.0.1:5500',
+  methods: ['GET', 'POST'],
+};
+
+app.use(cors(corsOptions));
+
+let isRecording = false;
+let videoChunks = [];
+
+const io = socketIo(server, {
+  cors: corsOptions,
+});
+
+io.on('connection', (socket) => {
+  socket.on('startRecording', () => {
+    isRecording = true;
+    videoChunks = [];
+  });
+
+  socket.on('chunk', (chunk) => {
+    if (isRecording) {
+      videoChunks.push(chunk);
+    }
+  });
+
+  socket.on('stopRecording', () => {
+    if (isRecording) {
+      isRecording = false;
+      io.emit('videoData', videoChunks);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
